155f40250d64cb0c702fe246bbe70ab7e238c479
Ryan Volz <ryan.volz@gmail.com>
2022-10-21 17:17:10 -0400

    Update recipe to use meson, required for version 15 and up.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-NOCONFIGURE=1 ./bootstrap.sh
-./configure --prefix=${PREFIX} \
-  --with-udev-rules-dir=${PREFIX}/lib/udev/rules.d \
-  --disable-systemd-daemon \
-  --disable-systemd-login \
-  --disable-systemd-login \
-  --disable-default-build-tests
-make -j ${CPU_COUNT}
-# tests fail to link on conda-forge CI for some reason,
-# though the link and run for me locally. -scopatz
-#XFAIL_TESTS="core-util-test thread-mainloop-test channelmap-test" make check
-make install
+meson_config_args=(
+    --buildtype=release
+    -D libdir=lib
+    --prefix=${PREFIX}
+    --wrap-mode=nodownload
+    -D database=simple
+    -D doxygen=false
+    -D udevrulesdir=${PREFIX}/lib/udev/rules.d
+)
+
+# MESON_ARGS is set by conda compiler activation script
+meson setup buildconda \
+    ${MESON_ARGS} \
+    "${meson_config_args[@]}" \
+    || (cat buildconda/meson-logs/meson-log.txt; false)
+meson compile -v -C buildconda -j ${CPU_COUNT}
+meson test -C buildconda --print-errorlogs --timeout-multiplier 10 --num-processes ${CPU_COUNT}
+meson install -C buildconda --no-rebuild
